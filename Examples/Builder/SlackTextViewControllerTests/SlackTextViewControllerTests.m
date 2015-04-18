0c3b2afd9bbfd721bce8ed6d569740f06b3abd34
Boris Bügling <boris@icculus.org>
2015-04-18 17:45:09 +0300

    Increase coverage with a few snapshot tests.

--- Examples/Builder/SlackTextViewControllerTests/SlackTextViewControllerTests.m
+++ Examples/Builder/SlackTextViewControllerTests/SlackTextViewControllerTests.m
@@ -22,6 +22,24 @@
 
 SpecBegin(SLKTextViewController)
 
+describe(@"SLKTextViewController input", ^{
+    __block SLKTextViewControllerTest *controller;
+
+    beforeEach(^{
+        controller = [[SLKTextViewControllerTest alloc] initWithTableViewStyle:UITableViewStylePlain];
+        controller.view.frame = [UIScreen mainScreen].bounds;
+    });
+
+    it(@"can become first responder", ^{
+        expect(controller).toNot.beNil();
+
+        [controller.textView becomeFirstResponder];
+
+        expect(controller.textView).toNot.beNil();
+        expect(controller.view).to.haveValidSnapshot();
+    });
+});
+
 describe(@"SLKTextViewController initialization", ^{
     it(@"has to be subclassed", ^{
         expect(^{
@@ -31,19 +49,30 @@ describe(@"SLKTextViewController initialization", ^{
 
     it(@"can be initialized with a table view style", ^{
         SLKTextViewControllerTest *controller = [[SLKTextViewControllerTest alloc] initWithTableViewStyle:UITableViewStylePlain];
+        controller.view.frame = [UIScreen mainScreen].bounds;
+
         expect(controller.tableView).toNot.beNil();
+        expect(controller.view).to.haveValidSnapshot();
     });
 
     it(@"can be initialized with a collection view layout", ^{
         UICollectionViewFlowLayout *layout = [UICollectionViewFlowLayout new];
+
         SLKTextViewControllerTest *controller = [[SLKTextViewControllerTest alloc] initWithCollectionViewLayout:layout];
+        controller.view.frame = [UIScreen mainScreen].bounds;
+
         expect(controller.collectionView).toNot.beNil();
+        expect(controller.view).to.haveValidSnapshot();
     });
 
     it(@"can be initialized with a scroll view", ^{
         UIScrollView *scrollView = [UIScrollView new];
+
         SLKTextViewControllerTest *controller = [[SLKTextViewControllerTest alloc] initWithScrollView:scrollView];
+        controller.view.frame = [UIScreen mainScreen].bounds;
+
         expect(controller.scrollView).toNot.beNil();
+        expect(controller.view).to.haveValidSnapshot();
     });
 
 });
